c685b41652ab43d7abd1025ed8088b4adbba5f0b
Patrick <prik.n@hotmail.de>
2021-05-05 18:21:36 +0200

    Label follows mouse position

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace ScreenOverlay
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace ScreenOverlay
 {
     partial class Form1
     {
@@ -27,22 +31,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(1450, 539);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(793, 95);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HELLO I AM LABEL";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LimeGreen;
-            this.TransparencyKey = System.Drawing.Color.LimeGreen;
             this.ClientSize = new System.Drawing.Size(3822, 2113);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.ResumeLayout(false);
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.LimeGreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
 
+            Timer myTimer = new Timer();
+            myTimer.Interval = 1000;
+            myTimer.Tick += new EventHandler(loop);
+            myTimer.Start();
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+        #endregion
+
+        private void loop(object sender, EventArgs e)
+        {
+            int x = Cursor.Position.X;
+            int y = Cursor.Position.Y;
+
+            this.label1.Location = new System.Drawing.Point(x,y);
         }
 
-        #endregion
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,7 @@ namespace ScreenOverlay
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
